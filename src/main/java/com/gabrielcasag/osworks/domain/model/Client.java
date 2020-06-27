536f17c8c204cbd9da1da0199fc93281ebd4e127
Gabriel Casagrande <gaspericasag@gmail.com>
2020-06-27 00:14:58 -0300

    Created Client entity

---
+++ src/main/java/com/gabrielcasag/osworks/domain/model/Client.java
@@ -0,0 +1,64 @@
+package com.gabrielcasag.osworks.domain.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+    @Column
+    private String email;
+    @Column
+    private String fone;
+    @Column
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFone() {
+        return fone;
+    }
+
+    public void setFone(String fone) {
+        this.fone = fone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return id.equals(client.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
