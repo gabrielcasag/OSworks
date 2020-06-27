dc8dffbae8c614589b692a91b2c0ed164adf2c3e
Gabriel Casagrande <gaspericasag@gmail.com>
2020-06-27 03:58:33 -0300

    Implemented browse operation of Client entity

--- src/main/java/com/gabrielcasag/osworks/api/controller/ClientController.java
+++ src/main/java/com/gabrielcasag/osworks/api/controller/ClientController.java
@@ -3,22 +3,33 @@ package com.gabrielcasag.osworks.api.controller;
 import com.gabrielcasag.osworks.domain.model.Client;
 import com.gabrielcasag.osworks.domain.repository.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.swing.text.html.parser.Entity;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@RequestMapping("/clients")
 public class ClientController {
 
     @Autowired
     private ClientRepository clientRepository;
 
-    @GetMapping("/clients")
+    @GetMapping
     public List<Client> list() {
         return clientRepository.findAll();
     }
+
+    @GetMapping("/{clientId}")
+    public ResponseEntity<Client> browse(@PathVariable Long clientId) {
+        Optional<Client> client = clientRepository.findById(clientId);
+
+        if ( client.isPresent()) {
+            return ResponseEntity.ok(client.get());
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
 }
